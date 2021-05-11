9516b6c907ee8567c1d16ed77563d81002a59a4a
alexarnoldy <alexarnoldy@gmail.com>
2021-05-11 16:24:36 -0700

    updates

---
+++ AWS/main.tf
@@ -0,0 +1,50 @@
+# Terraform configuration
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = var.instance_name_prefix
+  instance_count = 2
+
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+#module "website_s3_bucket" {
+#  source = "./modules/aws-s3-static-website-bucket"
+#
+#  bucket_name = "aarnoldy-20210509"
+#
+#  tags = {
+#    Terraform   = "true"
+#    Environment = "dev"
+#  }
+#}
---
+++ AWS/variables.tf
@@ -0,0 +1,64 @@
+# Input variable definitions
+
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "example-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_azs" {
+  description = "Availability zones for VPC"
+  type        = list(string)
+  default     = ["us-west-1a", "us-west-1b", "us-west-1c"]
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT gateway for VPC"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+variable "instance_name_prefix" {
+  description = "Name prefix for instances"
+  type	      = string
+  default     = "my-ec2-cluster"
+}
+
+variable "instance_ami" {
+  description = "AMI for the instance"
+  type	      = string
+  default     = "ami-05c558c169cfe8d99"
+}
+ 
+variable "instance_type" {
+  description = "Type for the instance"
+  type	      = string
+  default     = "t2.micro"
+}
